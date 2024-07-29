ef792610d8543578700ef03b803d0857f655ae16
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-29 08:20:26 -0500

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,3 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from models import db, Customer, CustomerAccount, Product, Order, OrderItem
+from schemas import customer_schema, customer_account_schema, product_schema, products_schema, order_schema, order_item_schema
+from datetime import datetime
+
+app = Flask(__name__)
+api = Api(app)
+
+# Initialize the SQLAlchemy object
+db.init_app(app)
+
 # Resources
 class CustomerResource(Resource):
     def post(self):
@@ -28,76 +40,10 @@ class CustomerResource(Resource):
         db.session.commit()
         return '', 204
 
-class CustomerAccountResource(Resource):
-    def post(self):
-        new_account = CustomerAccount(
-            username=request.json['username'],
-            password=request.json['password'],
-            customer_id=request.json['customer_id']
-        )
-        db.session.add(new_account)
-        db.session.commit()
-        return customer_account_schema.dump(new_account), 201
+# Add the resource to the API
+api.add_resource(CustomerResource, '/customer/<int:id>')
 
-    def get(self, id):
-        account = CustomerAccount.query.get(id)
-        return customer_account_schema.dump(account), 200
+# Continue with the other resources...
 
-    def put(self, id):
-        account = CustomerAccount.query.get(id)
-        account.username = request.json['username']
-        account.password = request.json['password']
-        db.session.commit()
-        return customer_account_schema.dump(account), 200
-
-    def delete(self, id):
-        account = CustomerAccount.query.get(id)
-        db.session.delete(account)
-        db.session.commit()
-        return '', 204
-
-class ProductResource(Resource):
-    def post(self):
-        new_product = Product(
-            name=request.json['name'],
-            price=request.json['price']
-        )
-        db.session.add(new_product)
-        db.session.commit()
-        return product_schema.dump(new_product), 201
-
-    def get(self, id):
-        product = Product.query.get(id)
-        return product_schema.dump(product), 200
-
-    def put(self, id):
-        product = Product.query.get(id)
-        product.name = request.json['name']
-        product.price = request.json['price']
-        db.session.commit()
-        return product_schema.dump(product), 200
-
-    def delete(self, id):
-        product = Product.query.get(id)
-        db.session.delete(product)
-        db.session.commit()
-        return '', 204
-
-class ProductListResource(Resource):
-    def get(self):
-        all_products = Product.query.all()
-        return products_schema.dump(all_products), 200
-
-class OrderResource(Resource):
-    def post(self):
-        new_order = Order(
-            customer_id=request.json['customer_id'],
-            order_date=datetime.now()
-        )
-        db.session.add(new_order)
-        db.session.commit()
-        return order_schema.dump(new_order), 201
-
-    def get(self, id):
-        order = Order.query.get(id)
-        return order_schema.dump(order), 200
+if __name__ == '__main__':
+    app.run(debug=True)
