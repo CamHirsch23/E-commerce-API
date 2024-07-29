565b654df159e6aed3806728fac34ea227ef96bb
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-29 08:15:39 -0500

    Update Schemas.py

--- Schemas.py
+++ Schemas.py
@@ -1,15 +1,13 @@
-#schemas/__init__.py
-
-
+from flask import Blueprint, request, jsonify
+from models import db, Customer, CustomerAccount, Product, Order, OrderItem
+from datetime import datetime
 from flask_marshmallow import Marshmallow
 
+# Initialize Marshmallow
 ma = Marshmallow()
 
 #schemas/customer_schema.py
-
-
-from . import ma
-from models.customer import Customer
+from models import Customer
 
 class CustomerSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -20,10 +18,7 @@ customer_schema = CustomerSchema()
 customers_schema = CustomerSchema(many=True)
 
 #schemas/customer_account_schema.py
-
-
-from . import ma
-from models.customer_account import CustomerAccount
+from models import CustomerAccount
 
 class CustomerAccountSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -34,10 +29,7 @@ customer_account_schema = CustomerAccountSchema()
 customer_accounts_schema = CustomerAccountSchema(many=True)
 
 #schemas/product_schema.py
-
-
-from . import ma
-from models.product import Product
+from models import Product
 
 class ProductSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -48,10 +40,7 @@ product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
 
 #schemas/order_schema.py
-
-
-from . import ma
-from models.order import Order
+from models import Order
 
 class OrderSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -62,10 +51,7 @@ order_schema = OrderSchema()
 orders_schema = OrderSchema(many=True)
 
 #schemas/order_item_schema.py
-
-
-from . import ma
-from models.order_item import OrderItem
+from models import OrderItem
 
 class OrderItemSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
@@ -74,3 +60,50 @@ class OrderItemSchema(ma.SQLAlchemyAutoSchema):
 
 order_item_schema = OrderItemSchema()
 order_items_schema = OrderItemSchema(many=True)
+
+#routes/__init__.py
+from flask import Blueprint
+
+customer_bp = Blueprint('customer_bp', __name__)
+customer_account_bp = Blueprint('customer_account_bp', __name__)
+product_bp = Blueprint('product_bp', __name__)
+order_bp = Blueprint('order_bp', __name__)
+
+# Order Routes
+@order_bp.route('/', methods=['POST'])
+def place_order():
+    pass  # Implementation
+
+@order_bp.route('/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    pass  # Implementation
+
+@order_bp.route('/orders', methods=['GET'])
+def get_orders():
+    try:
+        orders = Order.query.all()
+        result = orders_schema.dump(orders)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Main App
+from flask import Flask
+from models import db
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+ma.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def register_routes(app):
+    app.register_blueprint(customer_bp)
+    app.register_blueprint(customer_account_bp)
+    app.register_blueprint(product_bp)
+    app.register_blueprint(order_bp)
+    app.register_blueprint(order_item_bp)
